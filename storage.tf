3b078ee23c1c2214222062cfe8afbab8160fdfd1
Diego-Kingsley Ugoh <diegokingsley14@gmail.com>
2021-12-17 12:42:34 +0100

    Create Azure VM

--- storage.tf
+++ storage.tf
@@ -1,29 +1,29 @@
 variable "storage_account_name" {
-    type=string
-    default="diegoteststore"
+    type=string
+    default="diegoteststore"
 }
- 
+ 
 variable "resource_group_name" {
-    type=string
-    default="diego_test_grp"
+    type=string
+    default="diego_test_grp"
 }
- 
+ 
 provider "azurerm"{
 version = "=2.0"
 subscription_id = "ed869e72-a62e-4a1c-8133-92f57db9f18b"
-tenant_id       = "e5baf64e-2346-4edc-bbfe-bc3c3294a842"
+tenant_id       = "e5baf64e-2346-4edc-bbfe-bc3c3294a842"
 features {}
 }
- 
+ 
 resource "azurerm_resource_group" "grp" {
-  name     = var.resource_group_name
-  location = "North Europe"
+  name     = var.resource_group_name
+  location = "North Europe"
 }
- 
+ 
 resource "azurerm_storage_account" "store" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.grp.name
-  location                 = azurerm_resource_group.grp.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.grp.name
+  location                 = azurerm_resource_group.grp.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
